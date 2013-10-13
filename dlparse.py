420914496d9e4dba896745975df9cc18349ee960
phoeagon <phoeagon@gmail.com>
2013-10-13 13:25:57 +0800

    added collapse AST

--- dlparse.py
+++ dlparse.py
@@ -8,6 +8,8 @@ import sys
 import dllex
 import ply.yacc as yacc
 
+noCollapse = False;
+
 class Node:
 	def __init__(self,type,val_type,children=None):
 		 self.type = type
@@ -39,20 +41,32 @@ tokens = dllex.tokens
 def p_translation_unit(t):
 	'''translation_unit	: external_decl
 					| translation_unit external_decl'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("translation_unit","", t[1:] )
 	
 def p_external_decl(t):
 	'''external_decl : function_definition
 				| decl
 				'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("external_decl","", t[1:] )
 
 def p_function_definition(t):
 	'function_definition : type_spec  declarator  compound_stat'
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("function_definition","", t[1:] )
 
 def p_decl(t):
 	'decl	: type_spec declarator SEMI'
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("decl","", t[1:] )
 
 def p_decl_list(t):
@@ -60,6 +74,9 @@ def p_decl_list(t):
 	decl_list : decl 
 		| decl decl_list
 	'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("decl_list","", t[1:] )
 
 def p_type_spec_1(t):
@@ -68,11 +85,17 @@ def p_type_spec_1(t):
 				| INT 
 				| FLOAT
 	'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0] = Node( "type_spec" , "" , [ t[1] ] )
 	
 def p_type_spec_2(t):
 	'''type_spec	: struct_spec
 	'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0] = Node( "type_spec" , "" , [ t[1] ] )
 	
 def p_struct_spec(t):	
@@ -80,28 +103,43 @@ def p_struct_spec(t):
 				| STRUCT  LBRACE  struct_decl_list  RBRACE
 				| STRUCT ID
 	'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("struct_spec","", t[1:] )
 
 def p_struct_decl_list(t):
 	'''struct_decl_list : struct_decl 
 				| struct_decl_list  struct_decl
 	'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("struct_decl_list","", t[1:] )
 
 def p_struct_dec(t):
 	'struct_decl	: declarator_list  SEMI'
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("struct_decl","", t[1:] )
 
 def p_declarator_list(t):
 	'''declarator_list	: declarator
 				| declarator_list  COMMA  declarator
 				'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("declarator_list","", t[1:] )
 
 def p_declarator(t):
 	'''declarator	: TIMES  direct_declarator
 				| direct_declarator
 				'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("declarator","", t[1:] )
 
 def p_direct_declarator(t):
@@ -111,15 +149,24 @@ def p_direct_declarator(t):
 				| FUNCALL LBRACKET  direct_declarator  COLON  param_list  RBRACKET
 				| FUNCALL LBRACKET  direct_declarator  RBRACKET
 				'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("direct_declarator","", t[1:] )
 
 def p_param_list(t):
 	'''param_list	: param_decl
 				| param_list  COMMA  param_decl'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("param_list","", t[1:] )
 
 def p_param_decl(t):
 	'param_decl	: type_spec  declarator'
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("param_decl","", t[1:] )
 
 def p_stat(t):
@@ -128,6 +175,9 @@ def p_stat(t):
 				| selection_stat
 				| iteration_stat
 				| jump_stat'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("stat","", t[1:] )
 
 def p_exp_stat(t):
@@ -140,16 +190,25 @@ def p_compound_stat(t):
 				| LBRACE  stat_list  RBRACE
 				| LBRACE  decl_list  RBRACE
 				| LBRACE  RBRACE'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("compound_stat","", t[1:] )
 
 def p_stat_list(t):
 	'''stat_list	: stat
 				| stat_list  stat'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("stat_list","", t[1:] )
 
 def p_selection_stat(t):
 	'''selection_stat	: IF  LPAREN  exp  RPAREN  stat
 				| IF  LPAREN  exp  RPAREN  stat ELSE  stat'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("selection_stat","", t[1:] )
 
 def p_iteration_stat(t):
@@ -157,6 +216,9 @@ def p_iteration_stat(t):
 				| DO  stat  WHILE  LPAREN  exp  RPAREN  SEMI
 				| FOR  LPAREN  exp SEMI  exp  SEMI exp  RPAREN  stat
 				| FOREACH  LPAREN ID IN  stat  RPAREN  stat'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("iteration_stat","", t[1:] )
 
 def p_jump_stat(t):
@@ -164,22 +226,34 @@ def p_jump_stat(t):
 				| BREAK  SEMI
 				| RETURN  exp  SEMI
 				| RETURN	 SEMI'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("jump_stat","", t[1:] )
 
 def p_exp(t):
 	'''exp		: logical_exp
 				| unary_exp  EQUALS assignment_exp'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("exp","", t[1:] )
 
 def p_assignment_exp(t):
 	'''assignment_exp	: logical_exp
 			| unary_exp EQUALS assignment_exp'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("assignment_exp","", t[1:] )
 	
 def p_logical_exp(t):
 	'''logical_exp	: relational_exp
 				| logical_exp LOR relational_exp
 				| logical_exp LAND relational_exp'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("logical_exp","", t[1:] )
 
 def p_relational_exp(t):
@@ -191,12 +265,18 @@ def p_relational_exp(t):
 				| relational_exp EQ additive_exp
 				| relational_exp NE additive_exp
 				'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("relational_exp","", t[1:] )
 
 def p_additive_exp(t):
 	'''additive_exp	: mult_exp
 				| additive_exp PLUS mult_exp
 				| additive_exp MINUS mult_exp'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("additive_exp","", t[1:] )
 	pass
 
@@ -205,16 +285,25 @@ def p_mult_exp(t):
 				| mult_exp TIMES cast_exp
 				| mult_exp DIVIDE cast_exp
 				| mult_exp MOD cast_exp'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("mult_exp","", t[1:] )
 
 def p_cast_exp(t):
 	'''cast_exp	: unary_exp
 				| LPAREN type_spec RPAREN cast_exp'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("cast_exp","", t[1:] )
 
 def p_unary_exp(t):
 	'''unary_exp	: postfix_exp
 				| unary_operator cast_exp'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("cast_exp","", t[1:] )
 	
 def p_unary_operator(t):
@@ -222,6 +311,9 @@ def p_unary_operator(t):
 		| PLUS 
 		| MINUS 
 		| LNOT'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("unary_operator","", t[1:] )
 
 def p_postfix_exp(t):
@@ -230,6 +322,9 @@ def p_postfix_exp(t):
 				| FUNCALL LBRACKET  postfix_exp COLON argument_exp_list RBRACKET
 				| FUNCALL LBRACKET  postfix_exp  RBRACKET
 				| postfix_exp ARROW ID '''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("postfix_exp","", t[1:] )
 
 
@@ -238,24 +333,36 @@ def p_primary_exp(t):
 				| const 
 				| SCONST
 				| LPAREN  exp  RPAREN'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("primary_exp","", t[1:] )
 
 
 def p_argument_exp_list(t):
 	'''argument_exp_list : assignment_exp
 				| argument_exp_list COMMA assignment_exp'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("argument_exp_list","",[ t[1] ])
 
 def p_const(t):
 	'''const		: ICONST
 				| CCONST
 				| FCONST'''
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("const","",[ t[1] ])
 
 
 
 def p_empty(t):
 	'empty : '
+	if ( (len(t)==2) and (not noCollapse)):
+		t[0] = t[1];
+		return
 	t[0]=Node("empty","",[])
 
 
