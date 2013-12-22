80968c49dd38529c68bf58abe9b485427d550b83
John <john_zh_007@yahoo.com>
2013-12-22 10:44:28 +0800

    add new file for symtab

---
+++ dlcontext.py
@@ -0,0 +1,25 @@
+import sys
+import dllex
+import ply.yacc as yacc
+import copy 
+
+
+import dlparse 
+
+from dlcheck2 import *
+
+from dltrans import *
+
+
+if __name__ == "__main__":
+	outputFormat = 'json'
+	for arg in sys.argv:
+		if arg=="--xml":
+			outputFormat = 'xml'
+		elif arg=="--pobj":
+			outputFormat = 'pobj'
+    
+	from dlang import *
+	import dlang
+	walk( obj , context )
+        print context
